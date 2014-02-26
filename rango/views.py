3ce48c2f6775b8c3dfc0ebdc83d6ad9320cfa91c
unknown <mkakkad@DEVMKAKKAD.vistaprint.net>
2014-02-26 09:43:27 -0500

    Completed Section 5, entering extra excercises

--- rango/views.py
+++ rango/views.py
@@ -22,4 +22,7 @@ def index(request):
     #return HttpResponse("Rango says hello world!  <a href='/rango/about'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango Says: Here is the about page   <a href='/rango/'>Index</a>")
+    context = RequestContext(request)
+    return render_to_response('rango/about.html',context)
+
+
