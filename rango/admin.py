1af6dd7e7c209b6bbbdb9a8d6a79a6081d1109ca
manjay <manil.kakkad@gmail.com>
2014-02-26 13:18:55 -0500

    completeion of section 6, entering exercises

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
+#import admin.ModelAdmin
 
+# Admin Page : Category Configuration
 admin.site.register(Category)
-admin.site.register(Page)
+
+
+# Admin Page : Page Configuration
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category','url')   
+
+admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,8 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
     # Request the context of the request.
@@ -12,7 +14,14 @@ def index(request):
     
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold and from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    
+    # The following two lines are new
+    # We loop through each category returned, and create a URL attribute,
+    # This attribute stores an encoded URL (e.g. space replace with underscores)
+    for category in category_list:
+        category.url = category.name.replace(' ','_')
     
     #Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier
@@ -25,4 +34,38 @@ def about(request):
     context = RequestContext(request)
     return render_to_response('rango/about.html',context)
 
-
+def category(request, category_name_url):
+    # Request our context from the request passed to us
+    context = RequestContext(request)
+    
+    # Change underscores in the category name to spaces
+    # URLs don't handle spaces well, so we encode them as underscores
+    # We can then simply replace the underscores with spaces again to get the name
+    category_name = category_name_url.replace('_', ' ')
+    
+    # Create a context dictionary which we can pass to the template rendering engine.
+    # We start by containing the name of the category passed by the user
+    context_dict = {'category_name': category_name}
+    
+    try:
+        # Can we find a category with the given name?
+        # If we can't, the .get() method raises a DoesNotExist
+        # So the .get() method returns one model instance or raises an exceptoin.
+        category = Category.objects.get(name=category_name)
+        
+        # Retrieve all of the associated pages.
+        # Note that filter returns >= 1 model instance
+        pages = Page.objects.filter(category=category)
+        
+        #Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+        # We walso add the category object from the database to the context dictionary
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get there if we didn't find the specified category.
+        # Don't do anything - the template displays the "no category" message for us.
+        pass
+    
+    # Go render the response and return it to the client
+    return render_to_response('rango/category.html', context_dict, context)
