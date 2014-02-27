8371732b1c3058f3b520dcc3d3bdf513c7764ac5
manjay <manil.kakkad@gmail.com>
2014-02-26 22:34:53 -0500

    up to 7.2.7

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,8 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def index(request):
     # Request the context of the request.
@@ -15,8 +17,9 @@ def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
-    
+        
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
     # The following two lines are new
     # We loop through each category returned, and create a URL attribute,
     # This attribute stores an encoded URL (e.g. space replace with underscores)
@@ -69,3 +72,31 @@ def category(request, category_name_url):
     
     # Go render the response and return it to the client
     return render_to_response('rango/category.html', context_dict, context)
+
+def add_category(request):
+    # Get the context from the request.
+    context = RequestContext(request)
+    
+    # A HTTP POST?
+    if (request.method == 'POST'):
+        form = CategoryForm(request.POST)
+        
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form
+        form = CategoryForm()
+        
+        # Bad form (or form details), no form supplied...
+        # Render the form with error messages (if any). 
+    return render_to_response('rango/add_category.html', {'form': form}, context)  
+    
