23d56ccdb0c46a7de8c0a751c715d106d89cc217
manjay <manil.kakkad@gmail.com>
2014-02-27 17:24:25 -0500

    8.4.6

--- rango/admin.py
+++ rango/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 #import admin.ModelAdmin
-
-# Admin Page : Category Configuration
-admin.site.register(Category)
-
 
 # Admin Page : Page Configuration
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category','url')   
 
+
+# Admin Page : Category Configuration
+admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from rango.models import Category
-from rango.models import Page
+from rango.models import Category, Page, UserForm, UserProfileForm
 from rango.forms import CategoryForm, PageForm
 import os
 
@@ -152,13 +151,42 @@ def add_page(request, category_name_url):
                                 'category_name': category_name, 'form': form},
                                 context)
     
-
-#            
-#            return index(request)
-#        else:
-#            print form.erros
-#    else:
-#        form = PageForm()
-#    
-#    return render_to_response('rango/add_page.html', {'form': form}, context)
+def register(request):
+    # Like before, get the request's context
+    context = RequestContext(request)
     
+    registered = False
+    
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            
+            user.set_password(user.password)
+            user.save()
+            
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            
+            if 'picture in request.FILES':
+                profile.picture = request.FILES['picture']
+            
+            profile.save()
+            
+            registered = True
+            
+        else:
+            print user_form.errors, profile_form.errors
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    
+    return render_to_response(
+                'rango/register.html',
+                {'user_form': user_form,
+                 'profile_form': profile_form,
+                 'registered': registered},
+                context)
+        
